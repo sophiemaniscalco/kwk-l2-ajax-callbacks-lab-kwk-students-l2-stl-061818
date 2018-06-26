f5a5c8699bff66176e149229a09433f7a79e9cd2
sophiemaniscalco <sophmaniscalco@gmail.com>
2018-06-26 15:21:33 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,6 +1,19 @@
 $(document).ready(function (){
 });
 
-function searchRepositories(searchTerms){
-  $.get( "searchTerms.click" );
+function searchRepositories(){
+  const searchTerms = $( "#searchTerms").val();
+  $.get(`https://api.github.com/search/repositories?q=${searchTerms}`, data => {
+    $(`#results`).html(data);
+  })
+
+}
+
+var renderSearchResults = (data) => data.items.map(result => renderSearchResults(result))
+
+var renderSearchResults = (result) => {
+  return `
+    <div>
+    
+    </div>'
 }
